75406b746a8987fa25128155349090f81e803d79
Joe <jwataru@gmail.com>
2016-10-16 21:12:02 +1100

    Initial commit.

---
+++ index.ios.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class PropertyFinder extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+   color: 'black',
+   backgroundColor: 'white',
+   fontSize: 30,
+   margin: 80
+ }
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
