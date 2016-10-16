1cb84e9e426b884e897d1cf9c6e1b9a2781b5b09
Joe <jwataru@gmail.com>
2016-10-16 21:28:29 +1100

    Add NavigatorIOS

--- index.ios.js
+++ index.ios.js
@@ -8,15 +8,29 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text
+  Text,
+  NavigatorIOS
 } from 'react-native';
 
-class PropertyFinderApp extends Component {
+class HelloWorld extends Component {
   render() {
     return(
         <Text style={styles.text}>
           Hello world!
         </Text>
+    );
+  }
+}
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: HelloWorld,
+        }}/>
     );
   }
 }
@@ -27,6 +41,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
